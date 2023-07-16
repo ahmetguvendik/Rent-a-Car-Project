728ddb23edfee94cfe1b02195435b23b237c4956
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2023-07-16 15:52:56 +0300

    StartingProject

---
+++ RentaCarProject/Presentation/RentaCar.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentaCar.Presentation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
